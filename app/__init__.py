460f5e2280bacab189276e5b34dc166e65b6264e
Valéry Mertens <valerymertens@MacBook-Air-de-Valery.local>
2023-03-21 12:33:18 +0100

    modif de certains fichier mais fonctionne toujours po :(

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,9 @@
 from flask import Flask, render_template
-
 app = Flask(__name__)
 
-@app.route('/')
-def main():
-    return render_template('index.html')
-    
+@app.route("/")
+def accueil():
+  return render_template("index.html")
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+  app.run()
