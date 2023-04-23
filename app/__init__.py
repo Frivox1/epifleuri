ff24e3a4a0f0d98539061e4b5991feaa625ff58f
Spat <Nicolas.Lasch@gmail.com>
2023-04-23 13:49:27 +0200

    eds

--- app/__init__.py
+++ app/__init__.py
@@ -1,23 +1,23 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+def create_app(test_config=None):
+  # create and configure the app
+  app = Flask(__name__, instance_relative_config=True)
 
-@app.route("/")
-def accueil():
-  return render_template("index.html")
+  @app.route("/")
+  def accueil():
+    return render_template("index.html")
 
-@app.route("/produit")
-def prod():
-  return render_template("produit.html")
+  @app.route("/produit")
+  def prod():
+    return render_template("produit.html")
 
-@app.route("/evenement")
-def event():
-  return render_template("evenement.html")
+  @app.route("/evenement")
+  def event():
+    return render_template("evenement.html")
 
-@app.route("/informations")
-def info():
-  return render_template("informations.html")
+  @app.route("/informations")
+  def info():
+    return render_template("informations.html")
 
-
-if __name__ == "__main__":
-  app.run()
+  return app
